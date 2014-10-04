094c7d9b0ccc4d249c87f1a84ef07b28aca97a7f
Lukas <pelanek.lukas@seznam.cz>
2014-10-05 00:37:56 +0200

    prace na parseru

---
+++ src/scanner.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <malloc.h>
+#include <stdbool.h>
+
+
+typedef enum{
+
+	T_START,					// 00 - vychozi stav
+	T_IDENTIFICATOR,			// 01 - identifikator
+	T_COMMENT,					// 02 - komentar
+	T_END,						// 03 - konec cteni
+
+	// jeste jich sem prijde mnohem vic :)
+
+
+}tState;
+
+typedef struct{
+
+	tState state;		// informace o druhu tokenu
+	char *content;		// obsah tokenu
+	int row;			// radek na kterem se token nachazi
+	int length;			// delka tokenu
+
+}tToken;
+
+extern FILE *file;
+extern tToken token;
+
+tToken tGetToken();
+void tInitToken();
+void tFreeToken();
+void tExtentToken(int c);
